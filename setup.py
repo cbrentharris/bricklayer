545949040d5b2ce4e30da91383d160d4e0b81d49
Christopher Harris <chris@inflection>
2015-09-15 23:29:01 -0500

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Bricklayer',
+    version='1.0',
+    description='Lego Digital Designer Education Tool',
+    author='Christopher Harris',
+    author_email='cbrentharris@gmail.com',
+    url='http://wintercoding.club/',
+    packages=['distutils', 'distutils.command'],
+)
