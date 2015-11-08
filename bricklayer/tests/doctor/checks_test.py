a492a0c97e6bbe87050617b29d8cc8d622920f49
Christopher Harris <chharris@linkedin.com>
2015-11-07 22:52:03 -0600

    initial tests for checker

---
+++ bricklayer/tests/doctor/checks_test.py
@@ -0,0 +1,11 @@
+from bricklayer.doctor.checks import Checker
+from unittest import TestCase
+import os
+
+class ChecksTest(TestCase):
+    def test_executes_program(self):
+        checker = Checker()
+        filename = os.path.dirname(os.path.realpath(__file__)) + '/invalid_syntax.py'
+        print filename
+        checker.check_program(filename)
+        
