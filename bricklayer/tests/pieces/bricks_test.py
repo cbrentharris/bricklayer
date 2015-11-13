6194041738abc43ec7ae5b683ec5bdd66cf96a73
Christopher Harris <chharris@linkedin.com>
2015-11-12 21:54:50 -0600

    adding tests for bricks

---
+++ bricklayer/tests/pieces/bricks_test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from bricklayer.pieces.enums import *
+from bricklayer.pieces.bricks import Brick
+
+class BricksTest(TestCase):
+    u""""
+    Tests for the bricks class representation of the bricklayer internal bricks
+    """
+
+    def test_default_orientation_is_normal(self):
+        brick = Brick(Color.RED, Shape.BIT)
+        self.assertEqual(brick.orientation, Orientation.NORMAL)
