fa30c29bae049730c489f456d553cd693591a192
Christopher Harris <chharris@linkedin.com>
2016-02-25 15:13:10 -0600

    adding the colors file

---
+++ bricklayer/utils/colors.py
@@ -0,0 +1,18 @@
+class Colors:
+    class Foreground:
+        HEADER = '\033[95m'
+        BLUE = '\033[94m'
+        GREEN = '\033[92m'
+        WARNING = '\033[33m'
+        FAIL = '\033[91m'
+    class Background:
+        HEADER = '\033[45m'
+        BLUE = '\033[uguvldc
+        44m'
+        GREEN = '\033[42m'
+        WARNING = '\033[43m'
+        FAIL = '\033[41m'
+
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
