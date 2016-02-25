2035ea8b50d995023521c26ce61b74bb53aa8194
Christopher Harris <chharris@linkedin.com>
2016-02-25 16:31:52 -0600

    Adding a test to ensure it calls window url on windows

--- bricklayer/tests/utils/downloader_tests.py
+++ bricklayer/tests/utils/downloader_tests.py
@@ -4,12 +4,14 @@ import mock
 
 class DownloaderTest(TestCase):
 
-
     @mock.patch("urllib.urlretrieve")
     @mock.patch("platform.system", return_value="Darwin")
     def test_it_downloads_the_mac_zip(self, mock_system, mock_retrieve):
         Downloader.download_ldd()
         mock_retrieve.assert_called_with(Downloader.MAC_DOWNLOAD_URL, "ldd.zip")
 
-    def test_it_downloads_the_windows_exe(self):
-        pass
+    @mock.patch("urllib.urlretrieve")
+    @mock.patch("platform.system", return_value="Windows")
+    def test_it_downloads_the_windows_exe(self, mock_system, mock_retrieve):
+        Downloader.download_ldd()
+        mock_retrieve.assert_called_with(Downloader.WINDOWS_DOWNLOAD_URL, "ldd.exe")
