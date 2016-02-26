e02e403515c3cb12d08264a3bbf0dc7381252cc4
Christopher Harris <chharris@linkedin.com>
2016-02-25 21:12:12 -0600

    using the configurator to get the hostname

--- bricklayer/backend/settings.py
+++ bricklayer/backend/settings.py
@@ -1,3 +1,2 @@
 from bricklayer.doctor.config import Configurator
-BRICKLAYER_API_BASE_ENDPOINT = "http://localhost:8000"
-BRICKLAYER_API_METRICS_ENDPOINT = BRICKLAYER_API_BASE_ENDPOINT + "/metrics/" + Configurator.get_uuid()
+BRICKLAYER_API_METRICS_ENDPOINT = Configurator.get('General', 'hostname') + "/metrics/" + Configurator.get_uuid()
