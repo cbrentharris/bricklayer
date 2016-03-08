52f94baac1d7c32981ffcaa1d09f538ca0c8919c
Christopher Harris <chharris@linkedin.com>
2016-03-08 10:29:35 -0600

    Adding a progress bar to the downloader

--- bricklayer/utils/downloader.py
+++ bricklayer/utils/downloader.py
@@ -1,18 +1,33 @@
 import platform
 import urllib
 import subprocess
+from progressbar import ProgressBar
 
 class Downloader(object):
 
     WINDOWS_DOWNLOAD_URL = "http://cache.lego.com/downloads/ldd2.0/installer/setupLDD-PC-4_3_8.exe"
     MAC_DOWNLOAD_URL = "http://cache.lego.com/downloads/ldd2.0/installer/setupLDD-MAC-4_3_8.zip"
 
+    PB = None
+
     @classmethod
     def download_ldd(cls):
         if platform.system() == "Darwin":
-            urllib.urlretrieve(cls.MAC_DOWNLOAD_URL, "ldd.zip")
+            urllib.urlretrieve(cls.MAC_DOWNLOAD_URL, "ldd.zip", reporthook=cls.download_progress)
         elif platform.system() == "Windows":
             urllib.urlretrieve(cls.WINDOWS_DOWNLOAD_URL, "ldd.exe")
             # subprocess.Popen("ldd.exe")
 
+    @classmethod
+    def download_progress(cls, count, block_size, total_size):
+        if not cls.PB:
+            cls.PB = ProgressBar(maxval=total_size).start()
+        cls.PB.update(count * block_size)
 
+
+
+class Installer(object):
+
+    @classmethod
+    def install(cls):
+        pass
