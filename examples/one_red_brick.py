cfc2e23839822ad7463bd6a97de0572c05fad82e
Christopher Harris <chharris@linkedin.com>
2016-04-19 09:12:47 -0500

    adding example files

---
+++ examples/one_red_brick.py
@@ -0,0 +1,4 @@
+from bricklayer.levels.level_1 import put_2D_2x1_RED, output
+
+put_2D_2x1_RED(0,0)
+output()
---
+++ examples/red_and_blue.py
@@ -0,0 +1,6 @@
+from bricklayer.levels.level_1 import put_2D_2x4_RED, put_2D_4x2_BLUE, output
+
+put_2D_4x2_BLUE(0,0)
+put_2D_2x4_RED(0,2)
+
+output()
